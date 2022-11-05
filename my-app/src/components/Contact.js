67d9e538d98efa5caeb0c31f4ef964da45b1ceb6
Gloria <ubahgloria16@gmail.com>
2022-11-05 19:12:53 +0100

    Fix: Bug fix

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,9 +1,66 @@
-import React from "react"
-// import  { useState } from "react"
+import React, { useState } from "react"
 import Footer from "./Footer"
 
 const Contact = () => {
 
+     const [formData, setFormData] = useState({
+          firstname: "",
+          lastname: "",
+          email: "",
+          message: ""
+     })
+
+
+     const [formError, setFormError] = useState({})
+
+
+     const onChangeHandler = (event) => {
+          console.log(event)
+          if (event.target.checked === "checked"){
+               console.log("")
+          }
+          // setFormData(copy)
+          else {
+               setFormData(() => ({
+                    ...formData,
+                    [event.target.name]: event.target.value
+               }))
+          }
+     }
+
+
+
+     const validateForm = () => {
+          let err = {}
+          if (formData.firstname === ""){
+               err.firstname = "First name required!"
+          }
+
+           if (formData.lastname === ""){
+                err.lastname = "Last name required!"
+           }
+
+          if (formData.email === ""){
+               err.email = "Email required!"
+          }
+
+          if (formData.message === ""){
+               err.message= "Please enter a message"
+          }
+
+          setFormError({ ...err })
+     }
+
+     const onSubmitHandler = (event) => {
+          event.preventDefault()
+          console.log(formData)
+
+         let isValid = validateForm()
+         if (isValid){
+          alert("Submitted")
+         }
+     }
+     
 
      const name = "Gloria";
 
@@ -15,29 +72,59 @@ const Contact = () => {
                          <h1>Contact me</h1>
                          <p>Hi there, contact me to ask me about anything you have in mind.</p>
                          </div>
-               
-                         <form className="form">
+
+                         <form className="form" onSubmit={onSubmitHandler}>
 
                               <div className="input-group">
 
                                    <div className="group">
-                                        <label for="first_name">First Name</label>
-                                        <input type="text" id="first_name" placeholder="Enter your first name" required/>
+                                        <label htmlFor="first_name">First Name</label>
+                                        <input 
+                                        type="text" 
+                                        id="first_name" 
+                                        name="firstname"
+                                        placeholder="Enter your first name"
+                                        onChange={onChangeHandler}
+                                        value={formData.firstname}
+                                        />
+                                        <small>{formError.firstname}</small>
                                    </div>
 
                                    <div className="group">
-                                        <label for="last_name">Last Name</label>
-                                        <input type="text" id="last_name" placeholder="Enter your last name" required/>
+                                        <label htmlFor="last_name">Last Name</label>
+                                        <input
+                                         type="text" 
+                                         id="last_name" 
+                                         name="lastname"
+                                         placeholder="Enter your last name"
+                                         onChange={onChangeHandler}
+                                         value={formData.lastname}
+                                         />
+                                         <small>{formError.lastname}</small>
                                    </div>
                               </div>
 
-                              <label for="email">Email</label>
-                              <input type="email" id="email" placeholder="yourname@email.com" required />
+                              <label htmlFor="email">Email</label>
+                              <input 
+                              type="email" 
+                              id="email" 
+                              name="email"
+                              placeholder="yourname@email.com"
+                              onChange={onChangeHandler}
+                              value={formData.email}
+                              />
+                              <small>{formError.email}</small>
 
-                              <label for="message">Message</label>
+                              <label htmlFor="message">Message</label>
                               <textarea
                                id="message" 
-                               placeholder="Send me a message and I'll reply you as soon as possible..." ></textarea>
+                               placeholder="Send me a message and I'll reply you as soon as possible..."
+                               name="message" 
+                               onChange={onChangeHandler}
+                               value={formData.message}
+                               >
+                               </textarea>
+                               <small>{formError.message}</small>
                          
                               
                               <label className="checkbox"><p>You agree to providing your data to {name} who may contact you.</p>
